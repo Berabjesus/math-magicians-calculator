f6fbc86232adbed4ba3e0e41d510389efd5d70a6
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2021-01-26 20:43:57 +0300

    ADD application component files

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import Display from './Display'
+import ButtonPanel from './ButtonPanel'
+
+export default function App() {
+  return(
+    <>
+      <Display/>
+      <ButtonPanel/>
+    </>
+  )
+}
