48ee487b1bef79b435cb4218951852bd7fe870c4
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2021-02-09 00:40:09 +0300

    REMOVE aside from calculator display

--- src/components/display/calculator.js
+++ src/components/display/calculator.js
@@ -12,10 +12,7 @@ const Calculator = () => {
 
   return (
     <section className="d-flex pt-3">
-      <aside>
-        <h2>Lets do some math!</h2>
-      </aside>
-      <div className='h-100 ml-auto calc-container'>
+      <div className='h-100 mx-auto calc-container'>
         <Display result={state.next ? state.next : state.total} />
         <ButtonPanel clickHandler={handleClick} />
       </div>
